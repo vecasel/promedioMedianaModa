1a1a408685b0f0b12e4429b26c9f70254d1a513a
vecasel <danife1986@hotmail.com>
2022-05-09 15:01:49 -0500

    Se sube el proyecto completo

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var numerosPromedio = [];
 var numerosMediana = [];
+var numerosModa = [];
 var i = 0;
 var j = 0;
+var k = 0;
 var llenarArrayPromedio = document.getElementById("label2__button");
 var llenarArrayMediana = document.getElementById("mlabel2__button");
+var llenarArrayModa = document.getElementById("mdlabel2__button");
+
 
 llenarArrayPromedio.addEventListener("click", function(){
     if(document.getElementById("label2__input").value.length == 0){
@@ -25,7 +29,19 @@ llenarArrayMediana.addEventListener("click", function(){
         j++;
         document.getElementById("mlabel2__span").innerText = "Escoge el número " + (j + 1);
     }
-})
+});
+
+
+llenarArrayModa.addEventListener("click", function(){
+    if(document.getElementById("mdlabel2__input").value.length == 0){
+        alert("Debes colocar los números que deseas, para calcular la media")
+    }
+    else{
+        numerosModa[k] = parseInt(document.getElementById("mdlabel2__input").value);
+        k++;
+        document.getElementById("mdlabel2__span").innerText = "Escoge el número " + (k + 1);
+    }
+});
 
 
 function calcularPromedio(){
@@ -60,14 +76,30 @@ function calcularMediana(){
     else{
         var numerosMedianaInpar = parseInt(numerosMediana.length / 2);
         document.getElementById("result__idMedia").innerText = "La mediana de los números es: " + numerosMediana[numerosMedianaInpar];
-    }
-    
+    }   
+}
 
 
-    // const sumaTotal = numerosPromedio.reduce(function(valorAcumulado = 0, nuevoElemento){
-    //     return valorAcumulado + nuevoElemento;
-    // })
+function calcularModa(){
+    console.log(numerosModa);
+    document.getElementById("mdresult__salaries").innerText = numerosModa;
 
-    // const promedioTotal = sumaTotal / numerosPromedio.length;
-    // document.getElementById("result__idPromedio").innerText = "El promedio de los números es: " + promedioTotal.toFixed(2);
+    let numerosModaObject = {};
+
+    numerosModa.map(function(item){
+        if (numerosModaObject[item]){
+            numerosModaObject[item] += 1;
+        }
+        else{
+            numerosModaObject[item] = 1;
+        }
+    });
+
+    let numerosModaNew = Object.entries(numerosModaObject);
+
+    numerosModaNew.sort(function(a,b){
+        return a[1] - b[1]; 
+    });
+
+    document.getElementById("result__idModa").innerText = "La moda de la lista es: " + numerosModaNew[numerosModaNew.length - 1][0] + " ,el número se repite " + numerosModaNew[numerosModaNew.length - 1][1] + " veces."; 
 }
